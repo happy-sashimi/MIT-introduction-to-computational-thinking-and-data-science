3a07c7a883ff634784adfda500f28502b2f982f7
Natalia Galkina <notashek@gmail.com>
2024-11-01 16:59:45 +0100

    adding 2 lecture code

--- lect_1__greedy_algorithm.py
+++ lect_1__greedy_algorithm.py
@@ -58,9 +58,11 @@ def testGreedys(foods, maxUnits):
     testGreedy(foods, maxUnits, Food.density)
 
 
-names = ['wine', 'beer', 'pizza', 'burger', 'fries',
-         'cola', 'apple', 'donut', 'cake']
-values = [89,90,95,100,90,79,50,10]
-calories = [123,154,258,354,365,150,95,195]
+names = ['orange juice', 'potatoes', 'pizza margherita', 'pasta con tonno', 'apple',
+         'water', 'rice', 'chocolate']
+values = [89,90,95,100,90,79,50]
+calories = [123,154,258,354,365,150,95]
 foods = buildMenu(names, values, calories)
+
+
 testGreedys(foods, 1000)
